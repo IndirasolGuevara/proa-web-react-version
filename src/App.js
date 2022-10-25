f2bdc0a6f3fd0a8bb170d71b504342bb90565ea2
Indira Guevara <isgevara@escuelasproa.edu.ar>
2022-10-25 18:11:52 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import Header from './Components/Header/Header'
 import Cards from './Components/Cards/Cards';
 import Footer from './Components/Footer/Footer';
-
+/* se iportan los elementos que van a haber en app.js
+y se organizan 1°header 2°cards etc*/
 function App() {
   return (
     <div className="App">
