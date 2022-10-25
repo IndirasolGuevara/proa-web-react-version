6b34c38f81f701b0700866ae850d3e470212323a
Indira Guevara <isgevara@escuelasproa.edu.ar>
2022-10-25 18:11:42 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,34 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+/* NO TENGO QUE ENTENDERLO TODO XD*/ 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+/*se importa login */
+import Login from './Components/Login/Login';
+
+const router = createBrowserRouter([
+  /*para crear otras rutas. ej:la primera que aparece es app
+  la segunda es la de login y no se tiene que reenderizar como wikipedia
+  es como un dicionario de rutas */
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/",
+    element: <App/>
+  }
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
